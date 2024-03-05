ebfbe41929b9cf73782b3dd4187d2b42ae9f588a
feelonewong <feelonewong@gmail.com>
2024-03-06 07:17:39 +0800

    update:添加编程导航路由

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      Hello World~
+      Hello World~ <br />
+      <Link href="/users">users</Link> <br />
+      <a href="/users/new">new</a> <br />
     </main>
   )
 }
