955fc0faa76affa78f17401500dbd11b5146606d
feelonewong <feelonewong@gmail.com>
2024-03-13 07:32:17 +0800

    update:提交数据

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
-import React from "react"
+import React from "react";
 
 const NavBar = () => {
-    return (
-        <div className="flex bg-slate-200 p-3">
-            <Link href="/" className="mr-5">Next.js</Link>
-            <Link href="/users" >Users</Link>
-        </div>
-    )
-}
+  return (
+    <div className="flex bg-slate-200 p-3 space-x-3">
+      <Link href="/" className="mr-5">
+        Next.js
+      </Link>
+      <Link href="/users">Users</Link>
+      <Link href="/api/auth/signin">login</Link>
+    </div>
+  );
+};
 
 export default NavBar;
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,16 @@
 import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    
-})
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      httpOptions: {
+        timeout: 50000,
+      }
+    }),
+  ],
+});
 
-
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
